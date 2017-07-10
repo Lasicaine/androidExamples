f9495a9a25d12ec896929ac509d6ec203200e16d
Suomaa <a.antomonov@mail.ru>
2017-07-10 18:22:05 +0300

    Add project Calculator

---
+++ Calculator/app/src/main/java/rpb/suomaasoft/calculator/CalcActivity.java
@@ -0,0 +1,111 @@
+package rpb.suomaasoft.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class CalcActivity extends AppCompatActivity {
+
+    TextView resultField;
+    EditText numberField;
+    TextView operationField;
+    Double operand = null;
+    String lastOperation = "=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+        //get all fields from activity_calc.xml
+        resultField = (TextView) findViewById(R.id.resultField);
+        numberField = (EditText) findViewById(R.id.numberField);
+        operationField = (TextView) findViewById(R.id.operationField);
+    }
+
+    //Save state
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("OPERATION", lastOperation);
+        if(operand!=null)
+            outState.putDouble("OPERAND", operand);
+        super.onSaveInstanceState(outState);
+    }
+
+    //Get saved sate
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInsatnceState) {
+        super.onRestoreInstanceState(savedInsatnceState);
+        lastOperation = savedInsatnceState.getString("OPERATION");
+        operand = savedInsatnceState.getDouble("OPERAND");
+        resultField.setText(operand.toString());
+        operationField.setText(lastOperation);
+    }
+
+    //Numeric button click handler
+    public void onNumberClick(View view) {
+        Button button = (Button)view;
+        numberField.append(button.getText());
+
+        if(lastOperation.equals("=") && operand!=null){
+            operand = null;
+        }
+    }
+
+    //Operation button click handler
+    public void onOperationClick(View view){
+
+        Button button = (Button)view;
+        String op = button.getText().toString();
+        String number = numberField.getText().toString();
+
+        if(number.length()>0){
+            number = number.replace(',', '.');
+            try{
+                performOperation(Double.valueOf(number), op);
+            }catch (NumberFormatException ex){
+                numberField.setText("");
+            }
+        }
+        lastOperation = op;
+        operationField.setText(lastOperation);
+    }
+
+    private void performOperation(Double number, String operation){
+
+        if(operand == null){
+            operand = number;
+        }
+        else{
+            if(lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch(lastOperation){
+                case "=":
+                    operand =number;
+                    break;
+                case "/":
+                    if(number==0){
+                        operand =0.0;
+                    }
+                    else{
+                        operand /=number;
+                    }
+                    break;
+                case "*":
+                    operand *=number;
+                    break;
+                case "+":
+                    operand +=number;
+                    break;
+                case "-":
+                    operand -=number;
+                    break;
+            }
+        }
+        resultField.setText(operand.toString().replace('.', ','));
+        numberField.setText("");
+    }
+}
